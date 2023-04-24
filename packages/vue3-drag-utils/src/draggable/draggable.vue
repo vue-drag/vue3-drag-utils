48fd8ecb1e85170630d3781e38b0b0efcb86d96a
huyikai <hyk_tiger@foxmail.com>
2023-04-24 16:50:47 +0800

    :bulb: add comments

--- packages/vue3-drag-utils/src/draggable/draggable.vue
+++ packages/vue3-drag-utils/src/draggable/draggable.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 // Define emits
 const emit = defineEmits<{
-  (e: 'update:list', value: any): void;
+  (e: 'update:list', value: any): void; // Define the type of the emit function
 }>();
-type DropEffect = 'move' | 'copy';
+type DropEffect = 'move' | 'copy'; // Define the type of DropEffect
 // Define props
 const props = withDefaults(
   defineProps<{
@@ -21,9 +21,10 @@ const props = withDefaults(
   }
 );
 // Generate uuid
-const uuid = v4();
+const uuid = v4(); // Generate a unique id
 // Parse dropName
 const parser = (data: string | string[] | undefined) => {
+  // Define a function to parse dropName
   let array = [];
   if (typeof data === 'string') {
     array = [data, uuid];
@@ -36,60 +37,67 @@ const parser = (data: string | string[] | undefined) => {
 };
 // Compute typeName
 const typeName = computed(() => {
+  // Compute the typeName
   return props.dragName || uuid;
 });
 // Compute acceptName
 const acceptName = computed(() => {
+  // Compute the acceptName
   return parser(props.dropName);
 });
 const canDrag = computed(() => {
+  // Compute whether the item can be dragged
   return !props.disabled;
 });
 const dropEffect = computed(() => {
+  // Compute the drop effect
   return props.dropEffect;
 });
 // Provide typeName and acceptName
-provide('uuid', uuid);
-provide('typeName', typeName);
-provide('acceptName', acceptName);
-provide('canDrag', canDrag);
-provide('dropEffect', dropEffect);
+provide('uuid', uuid); // Provide the uuid
+provide('typeName', typeName); // Provide the typeName
+provide('acceptName', acceptName); // Provide the acceptName
+provide('canDrag', canDrag); // Provide whether the item can be dragged
+provide('dropEffect', dropEffect); // Provide the drop effect
 // Compute dragList
 const dragList = computed({
   get() {
     return props.list;
   },
   set(val) {
-    emit('update:list', val);
+    emit('update:list', val); // Emit the updated list
   }
 });
 
 const dragEnd = (data: any) => {
+  // Define the dragEnd function
   if (dropEffect.value === 'move' && !data.isSelf) {
-    dragList.value.splice(data.index, 1);
+    // If the drop effect is move and the item is not being dragged onto itself
+    dragList.value.splice(data.index, 1); // Remove the item from the list
   }
 };
 const dropMove = (
+  // Define the dropMove function
   item: any,
   dragIndex: number,
   hoverIndex: number,
   isSelf: boolean
 ) => {
-  const clone = [...dragList.value];
-  isSelf && clone.splice(dragIndex, 1);
-  clone.splice(hoverIndex, 0, item);
-  dragList.value = clone;
-  // isSelf && dragList.value.splice(dragIndex, 1);
-  // dragList.value.splice(hoverIndex, 0, item);
+  const clone = [...dragList.value]; // Clone the dragList
+  isSelf && clone.splice(dragIndex, 1); // If the item is being dragged onto itself, remove it from the list
+  clone.splice(hoverIndex, 0, item); // Insert the item at the hoverIndex
+  dragList.value = clone; // Update the dragList
 };
 
 const [mainDropCollect, drop] = useDrop(() => ({
-  accept: acceptName.value,
+  // Define the mainDropCollect and drop variables
+  accept: acceptName.value, // Set the accepted item types
   collect: (monitor: any) => ({
-    isOver: monitor.isOver()
+    // Collect information about the drop
+    isOver: monitor.isOver() // Check if the item is being dragged over the drop zone
   })
 }));
-provide('mainDropCollect', mainDropCollect);
+provide('mainDropCollect', mainDropCollect); // Provide the mainDropCollect variable
 </script>
 <template>
   <div :ref="drop">
