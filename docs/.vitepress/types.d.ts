b711e27b4293d473796584752708c0865d68cafd
huyikai <hyk_tiger@foxmail.com>
2023-04-26 15:02:06 +0800

    :label: update files types

--- docs/.vitepress/types.d.ts
+++ docs/.vitepress/types.d.ts
@@ -2,3 +2,10 @@ declare module '@vitepress-custom/vitepress-plugin-nav';
 declare module '@vitepress-custom/vitepress-plugin-fetch-docs';
 declare module '@vitepress-custom/vitepress-plugin-sidebar';
 declare module 'vitepress/dist/client/theme-default/composables/sidebar.js';
+declare module '*.vue' {
+  // 引入vue模块中ts的方法
+  import type { DefineComponent } from 'vue';
+  // 定义vue组件以及类型注解
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
