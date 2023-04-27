bcd68ebe65dfa1e65da1fa75876c5719cdba8c1e
huyikai <hyk_tiger@foxmail.com>
2023-04-27 15:31:30 +0800

    :rocket: test workflow

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -7,6 +7,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    base: './',
     server: {
       open: '/contents/Example/Simple.html'
     },
